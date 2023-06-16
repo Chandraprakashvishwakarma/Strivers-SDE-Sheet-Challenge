764b358191b153c868d5edd3ace4c0e4c87e372a
Chandraprakash Vishwakarma <74347870+Chandraprakashvishwakarma@users.noreply.github.com>
2023-06-16 18:45:17 +0530

    Update subset-sums.java

--- Subset Sums - GFG/subset-sums.java
+++ Subset Sums - GFG/subset-sums.java
@@ -1,38 +1,3 @@
-//{ Driver Code Starts
-//Initial Template for Java
-import java.io.*;
-import java.util.*;
-
-class GFG
-{
-    public static void main(String args[])throws IOException
-    {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        while(t-- > 0)
-        {
-            int N=sc.nextInt();
-			ArrayList<Integer> arr = new ArrayList<>();
-			for(int i = 0; i < N ; i++){
-			    arr.add(sc.nextInt());
-			}
-            Solution ob = new Solution();
-         
-            ArrayList<Integer> ans = ob.subsetSums(arr,N);
-            Collections.sort(ans);
-            for(int sum : ans){
-                System.out.print(sum+" ");
-            }
-            System.out.println();
-        }  
-    }
-}
-
-// } Driver Code Ends
-
-
-//User function Template for Java//User function Template for Java
-
 /*
 Q. no. 49: Subset Sums
 */
@@ -76,3 +41,37 @@ public class Solution {
     }
 }
 */
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0)
+        {
+            int N=sc.nextInt();
+			ArrayList<Integer> arr = new ArrayList<>();
+			for(int i = 0; i < N ; i++){
+			    arr.add(sc.nextInt());
+			}
+            Solution ob = new Solution();
+         
+            ArrayList<Integer> ans = ob.subsetSums(arr,N);
+            Collections.sort(ans);
+            for(int sum : ans){
+                System.out.print(sum+" ");
+            }
+            System.out.println();
+        }  
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java//User function Template for Java

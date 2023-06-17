36e0a6d4fb1b4ebd9fa8dae9472832a08421d148
Chandraprakash Vishwakarma <74347870+Chandraprakashvishwakarma@users.noreply.github.com>
2023-06-17 20:40:40 +0530

    Time: 915 ms (6.14%), Space: 44 MB (36.22%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -0,0 +1,52 @@
+/*
+Q. no. 55: Print Permutations - String
+*/
+class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        boolean [] used = new boolean[nums.length];
+        helper(nums,res,new ArrayList<Integer>(),used);
+        return res;
+    }
+    public void helper(int [] a,List<List<Integer>> res,List<Integer> temp,boolean [] used){
+        if(temp.size()==a.length && !res.contains(temp)){
+            res.add(new ArrayList<>(temp));
+            return;
+        }
+        for(int i=0;i<a.length;i++){
+            if(used[i] ) continue; 
+            used[i]=true;
+            temp.add(a[i]);
+            helper(a,res,temp,used);
+            used[i]=false;
+            temp.remove(temp.size()-1);
+        }
+        return;   
+    }
+}
+/*
+Codding Ninjas:
+
+import java.util.* ;
+import java.io.*; 
+public class Solution {
+    public static List<String> findPermutations(String s) {
+        List<String> res = new ArrayList<>();
+        boolean used[] = new boolean[s.length()];
+        helper(s,res,-1,"",used);
+        return res;
+    }
+    public static void helper(String s,List<String> res,int index,String temp,boolean [] used){
+        if(temp.length()==s.length()) {res.add(temp);return;}
+        for(int i=0;i<s.length();i++){
+            if(!used[i]){
+            used[i] =true;
+            temp+=(s.charAt(i));
+            helper(s,res,i,temp,used);
+            used[i]=false;
+            temp=temp.substring(0,temp.length()-1);
+            }
+        }
+    }
+}
+*/

204cb34da160e03fe863b3da51ea77a74d537a1c
Chandraprakash Vishwakarma <74347870+Chandraprakashvishwakarma@users.noreply.github.com>
2023-06-25 19:07:47 +0530

    Time: 2 ms (91.65%), Space: 40.8 MB (28.41%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,50 @@
+/*
+Q. no. 86: Palindrome Partitioning
+*/
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+
+        for(char ch: s.toCharArray()){
+            if(ch == '(' || ch == '[' || ch == '{') st.push(ch);
+            else if(!st.isEmpty()){
+                if(ch == ')' && st.peek()=='(') st.pop();
+                else if(ch == ']' && st.peek()=='[') st.pop();
+                else if(ch == '}' && st.peek()=='{') st.pop();
+                else return false;
+            }
+            else return false;
+        }
+        if(!st.isEmpty()) return false;
+        return true;
+    }
+}
+
+/*
+
+Codding ninjas:
+
+import java.util.*;
+public class Solution {
+    public static boolean isValidParenthesis(String expression) {
+        Stack<Character> st = new Stack<Character>();
+        for(int i = 0;i<expression.length();i++){
+            char c = expression.charAt(i);
+            
+            if(c =='{'||c=='['||c =='(') {
+                st.push(c);
+            }
+            else if(!st.isEmpty()){
+                if(c == ')' && st.peek() == '(') st.pop();
+                else if(c == ']' && st.peek() == '[') st.pop();
+                else if(c == '}' && st.peek() == '{') st.pop();
+                else return false;
+            }
+            else return false;
+        }
+        if(st.size()>0) return false;
+        return true;
+    }
+}
+
+*/
